b23ae59a69e3f09eed43a676dc4a39150c0b95c8
rogerzr <roger.zr@gmail.com>
2017-04-05 14:07:03 +0000

    Make widget accessible from formproperty

--- src/formelement.component.ts
+++ src/formelement.component.ts
@@ -64,6 +64,7 @@ export class FormElementComponent implements OnInit {
     let id = 'field' + (FormElementComponent.counter++);
 
     this.widget.formProperty = this.formProperty;
+    this.formProperty.widget = this.widget;
     this.widget.schema = this.formProperty.schema;
     this.widget.name = id;
     this.widget.id = id;
